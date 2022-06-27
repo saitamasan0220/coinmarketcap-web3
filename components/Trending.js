d1e215d27105be6a6615bf18c690b3ed2a423a30
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-06-27 17:05:36 -0400

    setting up Trending.js

---
+++ components/Trending.js
@@ -0,0 +1,46 @@
+import {useState} from 'react'
+import fire from '../assets/fire.png'
+import btc from '../assets/btc.png'
+import usdt from '../assets/usdt.png'
+import gainers from '../assets/gainers.png'
+import recent from '../assets/recent.png'
+import ReactSwitch from 'react-switch'
+
+const styles = {
+    trendingWrapper: `mx-auto max-w-screen-2xl`,
+    h1: `text-3xl text-white`
+}
+
+const Trending = () => {
+
+    const [checked, setChecked] = useState(false)
+
+    return (
+        <div className='text-white'>
+            <div className={styles.trendingWrapper}>
+                <div className="flex justify-between">
+                    <h1 className={styles.h1}>
+                        Today's Cryptocurrency by Market Cap
+                    </h1>
+
+                    <div className="flex">
+                        <p className="text-gray-400">Highlights &nbsp;</p>
+                        <ReactSwitch 
+                            checked={checked} 
+                            onChange={()=> {setChecked(!checked)}}
+                        />
+                    </div>
+                </div>
+                <br/>
+                <div className="flex">
+                    <p className='text-gray-400'>The global crypto market cap is $1.74T, a &nbsp;</p>
+                    <span>
+                        {/* <Rate /> */}
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Trending
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Trending from '../components/Trending'
 
 const Home: NextPage = () => {
   return (
