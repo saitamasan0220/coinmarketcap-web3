b456535facbfad2a64fbf48c712e1fdf966f99b4
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-06-28 15:02:15 -0400

    building CmcTable

---
+++ components/cmc-table/CmcTable.js
@@ -0,0 +1,31 @@
+import {useState, useEffect, useContext, useCallback} from 'react'
+import btc from '../../assets/btc.png'
+import { CoinMarketContext } from '../../context/context'
+
+
+const CMCTable = () => {
+
+    let {getTopTenCoins} = useContext(CoinMarketContext)
+    let [coinData, setCoinData] = useState(null)
+
+    const setData = useCallback(async () => {
+        try {
+            let apiResponse = await getTopTenCoins()
+            let filteredResponse = []
+
+            for (let i = 0; i < apiResponse.length; i++) {
+                const element = apiResponse[i]
+                if (element.cmc_rank <= 10) filteredResponse.push(element)
+            }
+
+            setCoinData(filteredResponse)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }, [getTopTenCoins])
+    return (
+        <div>CmcTable</div>
+    )
+}
+
+export default CMCTable
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Trending from '../components/Trending'
+import CMCTable from '../components/cmc-table/CmcTable'
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
       <div className="mt-10" />
       <Trending />
       <div className="mt-20" />
-      {/* <CMCtable /> */}
+      <CMCTable />
 
     </div>
   )
