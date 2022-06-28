878e14682873fd10303535c19c33346b8dd7e30a
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-06-28 14:51:54 -0400

    set up context

--- components/TrendingCardRow.js
+++ components/TrendingCardRow.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import Rate from './cmc-table/Rate'
+import Image from 'next/image'
+
+const styles = {
+    trendingCardRow: `flex items-center justify-between mb-4 text-[0.93rem]`
+}
 
 const TrendingCardRow = () => {
   return (
