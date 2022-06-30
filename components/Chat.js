dd3713f7cf303394ed379ca4679516277fd81ec1
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-06-30 14:16:11 -0400

    chat css

--- components/Chat.js
+++ components/Chat.js
@@ -6,8 +6,18 @@ import Image from 'next/image'
 import Button from './Button'
 
 const styles = {
-    bullishLabel: ``,
-    bearishLabel: ``,
+    bullishLabel: `flex cursor-pointer active:bg-green-600 items-center text text-green-600 border border-green-600 h-min px-2 rounded-lg`,
+    bearishLabel: `flex cursor-pointer active:bg-red-500 items-center text-[#EA3943] border border-red-600 h-min px-2 rounded-lg`,
+    input: `w-full bg-[#323546] p-4 outline-none rounded-xl`,
+    chatContainer: `p-5 bg-[#222531] rounded-xl max-h-ful`,
+    flexBetween: `flex justify-between`,
+    flexCenter: `flex justify-center items-center`,
+    chat: `max-w-lg min-w-full`,
+    postButtonContainer: `flex align-center justify-end`,
+    boldText: `font-bold`,
+
+    activeBullishLabel: `flex cursor-pointer bg-green-600 items-center text-white border border-green-600 h-min px-2 rounded-lg`,
+    activeBearishLabel: `flex cursor-pointer bg-red-500 items-center text-white border border-red-600 h-min px-2 rounded-lg`,
 }
 
 const Chat = () => {
