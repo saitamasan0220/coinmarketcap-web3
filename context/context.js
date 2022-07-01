aa63e6ce0a1d00d7e9aa8b73012965be1a96c700
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-07-01 07:10:36 -0400

    setting up context

--- context/context.js
+++ context/context.js
@@ -1,8 +1,39 @@
 import {createContext, useState, useEffect} from 'react'
+import {useMoralis} from 'react-moralis'
+import {useMoralisQuery} from 'react-moralis'
+
+import {
+    dogeAbi,
+    daiAbi,
+    linkAbi,
+    usdcAbi,
+    dogeAddress,
+    linkAddress,
+    daiAddress,
+    usdcAddress,
+} from '../lib/constants'
 
 export const CoinMarketContext = createContext()
 
 export const CoinMarketProvider = ({children}) => {
+
+    const {isAuthenticated, user, Moralis} = useMoralis()
+
+    // useMoralisQuery()
+
+    const [currentAccount, setCurrentAccount] = useState('')
+    const [openBuyCryptoModal, setOpenbuyCryptoModal] = useState(false)
+    const [fromToken, setFromToken] = useState('')
+    const [toToken, setToToken] = useState('')
+    const [amount, setAmount] = useState('')
+
+    useEffect(() => {
+        if(isAuthenticated) {
+            const account = user.get('ethAddress')
+            setCurrentAccount(account)
+        }
+    }, [isAuthenticated])
+
     
     const getTopTenCoins = async () => {
         try {
